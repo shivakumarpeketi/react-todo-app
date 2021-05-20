d70c991491d2ee09bf0c5f3ad5d86d46680d8dcb
Siva Kumar <siva.peketi@resolver.com>
2021-05-20 17:17:45 +0530

    logs added to explore useSelector behavior

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,16 +2,25 @@ import React from 'react'
 import TodoItem from './TodoItem'
 import { useSelector } from 'react-redux'
 
-function TodoList() {
-    let todos = useSelector(state=>state);
+const TodoList = () => {
+    let todos = useSelector(state=>{
+        console.log(`Todo List Selector`);
+        return state;
+    });
+    // console.dir(todos)
 
     return (
         <div className="my-4">
+            {logger()}
             {todos.map((todo)=>   {
                 return <TodoItem key={todos.id} todo={todo}/>;
             })}
         </div>
     )
 }
+
+const logger = () => console.log(`Todo List Render`);
+
+console.log(`Todo List`)
 
 export default TodoList
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -6,27 +6,34 @@ export let reducer = (state = todos, action) => {
     let newTodos;
     switch (action.type) {
         case ADD_TODO:
-            newTodos = [...state];
+            // console.dir(state)
+            // console.dir({...state})
+            // console.dir([...state])
+            newTodos = [...state]
             newTodos.push(action.payload);
-            return newTodos;
+            console.log(newTodos===state)
+            return [...state, action.payload];
         case DELETE_TODO:
             newTodos = [...state];
-            newTodos = newTodos.filter(todo => todo.id != action.payload);
+            newTodos = newTodos.filter(todo => todo.id !== action.payload);
             return newTodos;
         case UPDATE_TODO:
             newTodos = [...state];
             let index = -1;
             for (let i = 0; i < newTodos.length; i++) {
                 index++;
-                if (newTodos[i].id == action.payload.id) {
+                if (newTodos[i].id === action.payload.id) {
                     break;
                 }
 
             }
-            if (index != -1) {
+            if (index !== -1) {
                 newTodos[index] = action.payload;
                 return newTodos;
             }
+            break;
+            default:
+                return state;
 
     }
     return state;
